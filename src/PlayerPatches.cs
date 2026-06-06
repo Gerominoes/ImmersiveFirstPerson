3f64f133f5d211b8628bf25587b453f899606b1d
Melle Jefferson Larua <mmlarua@gmail.com>
2026-06-06 19:24:08 +0800

    Keep player patch limited to first person state

---
+++ src/PlayerPatches.cs
@@ -0,0 +1,13 @@
+using HarmonyLib;
+
+namespace ImmersiveFirstPerson;
+
+[HarmonyPatch(typeof(Player))]
+[HarmonyPatch("Update")]
+internal static class PlayerUpdatePatch
+{
+    private static void Postfix(Player __instance)
+    {
+        FirstPersonState.Update(__instance);
+    }
+}
