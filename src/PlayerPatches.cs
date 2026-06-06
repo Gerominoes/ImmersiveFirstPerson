e56f5614400a854b3e40e1e43dfc799dcdbd3848
Melle Jefferson Larua <mmlarua@gmail.com>
2026-06-06 18:43:00 +0800

    Apply body visibility restore during player update

---
+++ src/PlayerPatches.cs
@@ -0,0 +1,15 @@
+using HarmonyLib;
+
+namespace ImmersiveFirstPerson;
+
+[HarmonyPatch(typeof(Player))]
+[HarmonyPatch("Update")]
+internal static class PlayerUpdatePatch
+{
+    private static void Postfix(Player __instance)
+    {
+        FirstPersonState.Update(__instance);
+        BodyVisibilityController.Update(__instance);
+        HeadVisibilityController.Update(__instance);
+    }
+}
